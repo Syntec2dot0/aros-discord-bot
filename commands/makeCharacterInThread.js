ca9e17d6b2267d0929cc63ce460976cdb582decf
Julius Kamp <juliuskamp2000@gmail.com>
2021-12-09 23:07:25 +0100

    add functionality test threads

--- commands/makeCharacterInThread.js
+++ commands/makeCharacterInThread.js
@@ -1,5 +1,4 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { rolladiefunc, gearfunc, indent } from '../util.js';
 
 let command = {
 	data: new SlashCommandBuilder()
@@ -7,7 +6,21 @@ let command = {
 		.setDescription('Erstellt einen Charakter im eigenen Thread'),
 
 	async execute(interaction) {
+		await interaction.reply("Ich erstelle einen thread für dich");
 
+		const thread = await interaction.channel.threads.create({
+			name: 'Charaktererschaffung für ' + interaction.member.displayName,
+			autoArchiveDuration: 60,
+		});
+
+		interaction.client.on("messageCreate", (message) => {
+			if (message.channel.id == thread && message.author.id == interaction.member.user.id) {
+				console.log(message);
+				thread.send("test");
+				interaction.deleteReply();
+			}
+
+		});
 	},
 };
 
